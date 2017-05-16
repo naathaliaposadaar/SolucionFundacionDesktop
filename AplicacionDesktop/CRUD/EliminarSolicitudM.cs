fa59cf6b0b98e060a169a7d46129f7ca10a7c214
naathaliaposadaar <naathaliaposada@gmail.com>
2017-05-16 00:35:25 -0400

    NP
    Ingresar Inventario

---
+++ AplicacionDesktop/CRUD/EliminarSolicitudM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionDesktop.MENU
+{
+    public partial class EliminarSolicitudM : Form
+    {
+        public EliminarSolicitudM()
+        {
+            InitializeComponent();
+        }
+
+        private void EliminarSolicitudM_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AplicacionDesktop/CRUD/IngresarInventario.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace AplicacionDesktop.MENU
+{
+    partial class IngresarInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdProducto = new System.Windows.Forms.RadioButton();
+            this.rdMedicina = new System.Windows.Forms.RadioButton();
+            this.lblIngresarInventario = new System.Windows.Forms.Label();
+            this.cbxProducto = new System.Windows.Forms.ComboBox();
+            this.cbxMedicina = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtObservaciones = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rdProducto
+            // 
+            this.rdProducto.AutoCheck = false;
+            this.rdProducto.AutoSize = true;
+            this.rdProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdProducto.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.rdProducto.Location = new System.Drawing.Point(40, 79);
+            this.rdProducto.Name = "rdProducto";
+            this.rdProducto.Size = new System.Drawing.Size(99, 24);
+            this.rdProducto.TabIndex = 0;
+            this.rdProducto.TabStop = true;
+            this.rdProducto.Text = "Producto";
+            this.rdProducto.UseVisualStyleBackColor = true;
+            this.rdProducto.CheckedChanged += new System.EventHandler(this.rdProducto_CheckedChanged);
+            this.rdProducto.Click += new System.EventHandler(this.rdProducto_Click);
+            // 
+            // rdMedicina
+            // 
+            this.rdMedicina.AutoCheck = false;
+            this.rdMedicina.AutoSize = true;
+            this.rdMedicina.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdMedicina.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.rdMedicina.Location = new System.Drawing.Point(295, 79);
+            this.rdMedicina.Name = "rdMedicina";
+            this.rdMedicina.Size = new System.Drawing.Size(98, 24);
+            this.rdMedicina.TabIndex = 1;
+            this.rdMedicina.TabStop = true;
+            this.rdMedicina.Text = "Medicina";
+            this.rdMedicina.UseVisualStyleBackColor = true;
+            this.rdMedicina.CheckedChanged += new System.EventHandler(this.rdMedicina_CheckedChanged);
+            this.rdMedicina.Click += new System.EventHandler(this.rdMedicina_Click);
+            // 
+            // lblIngresarInventario
+            // 
+            this.lblIngresarInventario.AutoSize = true;
+            this.lblIngresarInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngresarInventario.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblIngresarInventario.Location = new System.Drawing.Point(210, 9);
+            this.lblIngresarInventario.Name = "lblIngresarInventario";
+            this.lblIngresarInventario.Size = new System.Drawing.Size(183, 24);
+            this.lblIngresarInventario.TabIndex = 2;
+            this.lblIngresarInventario.Text = "Ingresar Inventario";
+            // 
+            // cbxProducto
+            // 
+            this.cbxProducto.FormattingEnabled = true;
+            this.cbxProducto.Location = new System.Drawing.Point(40, 110);
+            this.cbxProducto.Name = "cbxProducto";
+            this.cbxProducto.Size = new System.Drawing.Size(200, 21);
+            this.cbxProducto.TabIndex = 3;
+            // 
+            // cbxMedicina
+            // 
+            this.cbxMedicina.FormattingEnabled = true;
+            this.cbxMedicina.Location = new System.Drawing.Point(295, 109);
+            this.cbxMedicina.Name = "cbxMedicina";
+            this.cbxMedicina.Size = new System.Drawing.Size(200, 21);
+            this.cbxMedicina.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(36, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(36, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fecha";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(295, 156);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(200, 20);
+            this.txtCantidad.TabIndex = 7;
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(295, 197);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 8;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnIngresar.Location = new System.Drawing.Point(413, 305);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(82, 32);
+            this.btnIngresar.TabIndex = 9;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolver.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnVolver.Location = new System.Drawing.Point(535, 305);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(79, 31);
+            this.btnVolver.TabIndex = 10;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(37, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Observaciones";
+            // 
+            // txtObservaciones
+            // 
+            this.txtObservaciones.Location = new System.Drawing.Point(295, 234);
+            this.txtObservaciones.Multiline = true;
+            this.txtObservaciones.Name = "txtObservaciones";
+            this.txtObservaciones.Size = new System.Drawing.Size(200, 43);
+            this.txtObservaciones.TabIndex = 12;
+            // 
+            // IngresarInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 347);
+            this.Controls.Add(this.txtObservaciones);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxMedicina);
+            this.Controls.Add(this.cbxProducto);
+            this.Controls.Add(this.lblIngresarInventario);
+            this.Controls.Add(this.rdMedicina);
+            this.Controls.Add(this.rdProducto);
+            this.Name = "IngresarInventario";
+            this.Text = "IngresarInventario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdProducto;
+        private System.Windows.Forms.RadioButton rdMedicina;
+        private System.Windows.Forms.Label lblIngresarInventario;
+        private System.Windows.Forms.ComboBox cbxProducto;
+        private System.Windows.Forms.ComboBox cbxMedicina;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtObservaciones;
+    }
+}
